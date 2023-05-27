083c3803bb4517dbd3651ea1f62b179fcf6c0f17
lakshmimeghana666 <119840524+lakshmimeghana666@users.noreply.github.com>
2023-05-27 19:14:49 +0530

    def prime(n):     c=0     for i in range(1,n+1):         if n%i==0:             c+=1     return c n=int(input()) if prime(n)==2:     print("prime") else:     print("not a prime")

--- prime_number.py
+++ prime_number.py
@@ -1,9 +1,11 @@
+def prime(n):
+    c=0
+    for i in range(1,n+1):
+        if n%i==0:
+            c+=1
+    return c
 n=int(input())
-c=0
-for i in range(1,n+1):
-    if n%i==0:
-        c+=1
-if c==2:
+if prime(n)==2:
     print("prime")
 else:
     print("not a prime")
